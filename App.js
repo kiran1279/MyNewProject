f32142a0c8115809820369a41c414d0c6a00c4d4
Kiran kumar <kirankumarmallavarapu1@gmail.com>
2024-07-14 11:56:33 +0530

    changes

--- App.js
+++ App.js
@@ -1,9 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Platform, StyleSheet, Text, View, Alert } from 'react-native'
+import React, { useEffect } from 'react'
 import StackNavigation from './src/Navigation/StackNavigation'
 import { NavigationContainer } from '@react-navigation/native'
-
+import {
+  checkNotifications,
+  requestNotifications,
+  RESULTS,
+  openSettings,
+} from 'react-native-permissions';
 const App = () => {
+
+  useEffect(() => {
+    const requestUserPermission = async () => {
+      if (Platform.OS === 'ios') {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+          authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+          authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+        if (enabled) {
+          console.log('Authorization status:', authStatus);
+        } else {
+          Alert.alert('Permission denied', 'You need to enable notifications to receive alerts.');
+        }
+      } else if (Platform.OS === 'android') {
+        const { status } = await checkNotifications();
+        if (status === RESULTS.DENIED) {
+          const { status: newStatus } = await requestNotifications(['alert', 'badge', 'sound']);
+          if (newStatus === RESULTS.GRANTED) {
+            console.log('Notification permission granted.');
+          } else {
+            Alert.alert(
+              'Notification Permission Denied',
+              'You have disabled notifications. Please enable them from settings.',
+              [
+                {
+                  text: 'Cancel',
+                  style: 'cancel',
+                },
+                {
+                  text: 'Open Settings',
+                  onPress: () => openSettings(),
+                },
+              ],
+            );
+          }
+        }
+      }
+    };
+
+    requestUserPermission();
+  }, []);
+
   return (
     <NavigationContainer>
       <StackNavigation />
